7549d2cd4fa6869ce8920a1f46b60ca827a35df8
stephanielevon <levon@igbmc.fr>
2015-06-24 10:07:08 +0200

    Ajout informations README et commentaires script

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # script_qc
 
 Ce script a pour but de réaliser un contrôle qualité de lectures issues de séquençage d'ADN grâce à l'outil BEDTools. Au sein de la plateforme, la plupart des analyses de séquençage d'ADN sont des captures. 
-Deux fichiers d'entrée sont nécessaires : 
-	- un fichier BAM contenant les lectures alignées sur le génome de référence
+Deux fichiers d'entrée sont nécessaires : <\br>
+	- un fichier BAM contenant les lectures alignées sur le génome de référence <\br>
 	- un fichier BED contenant les coordonnées de capture. 
 
-Utilisation : perl script_qc --bam <fichier.bam> --bed <fichier.bed> 
+Utilisation : perl script_qc.pl --bam <\fichier.bam> --bed <\fichier.bed> 
 
 
+La commande BEDTools coverage génére un fichier au format tsv qui s'intitule : nom_fichier_bam.coverage.tsv. 
+Celui-ci contient 6 colonnes : le numéro du chromosome, la position de début et de fin de la région d'intérêt, le nombre de lectures qui s'y alignent, la taille de la région d'intérêt, la taille de la région d'intérêt couverte par au moins une lecture et le ratio de couverture, c'est-à-dire la proportion de la région qui est couverte par au moins une lecture. 
 
-Parameters
-Tools
-Statistics::R
-Time request ? 
+Le module Statistics::R prend ensuite les colonnes de ratio de couverture et du nombre de lectures qui s'alignent par région pour générer deux graphiques, un histogramme et un graphiques de boîtes à moustache. Le fichier créér a pour nom : nom_fichier_bam.plot.pdf.
--- script_qc.pl
+++ script_qc.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl # Variable d'environnement. Indique que l'interpreteur est Perl, celui-ci sera utilisé quelque soit sa localisation. 
 
 
 # Date : 13/04/15
-# Author : Levon Stephanie 
-# Purpose : utiliser l'outil BEDtools et la commande "coverage". Nécessite deux fichiers : le fichier .bam (reads alignés) 
+# Auteur : Levon Stephanie 
+# But : utiliser l'outil BEDtools et la commande "coverage". Nécessite deux fichiers : le fichier .bam (lectures alignées) 
 # et le fichier .bed (coordonnées des capture). 
 
 use strict;
@@ -18,32 +18,30 @@ use Statistics::R;
 
 my $desc = "\n Deux arguments requis : --bam <in.bam> --bed <in.bed> \n\n";
 
-my %options = (); # Toutes options données en ligne de commande sont enregistrées dans un tabelau associatif
+my %options = (); # Toutes les options données en ligne de commande sont enregistrées dans un tabelau associatif
 my $nbArguments = @ARGV;
 
 GetOptions ("bam=s"   => \$options{"bam_file"}, 
             "bed=s"  => \$options{"bed_file"})
-# si help est initialisé print l'aide 
-               
+        
  or die($desc);
 
 if ($nbArguments != 4){
 	print $desc;
 	exit
 }
-#pod2usage( # print a usage message from embedded pod documentation
-#	)if (defined$options{"help"} || $nbArguments == 0);
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
-# Exécuter une commande bash avec l'outil BEDtools coverage, rediriger les résultats dans un fchier output portant le nom du fichier bed .coverage.tsv
+# Exécuter une commande bash avec l'outil BEDtools coverage, rediriger les résultats dans un fchier output portant le nom du fichier bam .coverage.tsv
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-my @output = split(/\./, $options{"bed_file"}); # Se sert du nom de fichier entré par l'utilisateur pour nommer le nouveau fichier
+my @output = split(/\./, $options{"bam_file"}); # Se sert du nom de fichier entré par l'utilisateur pour nommer le nouveau fichier qui contiendra les résultats de la commande BEDTools coverage
 my $bedtools_res = $output[0].".coverage.tsv"; 
-system (" bedtools coverage -b $options{bed_file} -abam $options{bam_file} > $bedtools_res "); # Place dans un fichier de sortie 
+# La commande suivante prend en paramètre le fichier bed (-b) et le fichier bam (-abam)
+system (" bedtools coverage -b $options{bed_file} -abam $options{bam_file} > $bedtools_res "); # Place le résultat dans un fichier de sortie qui prend le nom du fichier et ajoute en préfixe ".coverage.tsv"
 my $bedtools_sorted = $output[0].".coverage_sorted.tsv"; 
-system("sed 's/^chr//g' $bedtools_res | sort -k 1,1n  > $bedtools_sorted"); # | awk '{print \"chr\"\$0}';
+system("sed 's/^chr//g' $bedtools_res | sort -k 1,1n  > $bedtools_sorted"); # Permet d'obtenir un fichier trié (optionnel)
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -54,10 +52,10 @@ my $R = Statistics::R->new();
 $R -> startR() ; # Ouvrir une session R 
 
 my @output_plot = split(/\./, $bedtools_res);
-$R->run(qq`pdf("$output_plot[0].plot.pdf")`); # Lancer la commande R qui va créer notre fichier .pdf trier 
+$R->run(qq`pdf("$output_plot[0].plot.pdf")`); # Lancer la commande R qui va créer notre fichier .pdf et le nommer avec le nom du fichier bam suivit de ".plot.pdf"
 
 my$plot_file = $R -> run (
-	qq`data <- read.table("/Users/stephanie/Desktop/fichier_test/$bedtools_res", header=F)`,
+	qq`data <- read.table("/Users/stephanie/Documents/script_qc_bedtools/$bedtools_res", header=F)`, # Va chercher le fichier a son emplacement
 	
 	q`chr <- data[, 1]`,
 	q`chr = factor(chr,levels(chr)[c(1, 11, 15:20, 2:10, 12:14, 21, 22)])`,
@@ -75,7 +73,5 @@ my$plot_file = $R -> run (
 
 $R->stopR(); # Fermer la session R et l'interaction avec Perl
 
-#if (-e $output_plot[0].plot.pdf){
-#print "Fichiers créés : \n - $bedtools_res \n - $output_plot[0].plot.pdf \n";
-#}
+
 
